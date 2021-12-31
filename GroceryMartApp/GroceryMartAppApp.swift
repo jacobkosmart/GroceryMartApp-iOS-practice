bc4c2f0783be8ebd4b4b31d5f54028173a80205e
jacobkosmart <jacobkosmart@gmail.com>
2021-12-31 15:57:50 +0900

    Initial Commit

---
+++ GroceryMartApp/GroceryMartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryMartAppApp.swift
+//  GroceryMartApp
+//
+//  Created by Jacob Ko on 2021/12/31.
+//
+
+import SwiftUI
+
+@main
+struct GroceryMartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
